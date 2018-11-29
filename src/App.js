4cddcd3e4ba0a4f3fbbc9400b96bce00d9eb0906
cyardimci <cyardimci@sabanciuniv.edu>
2018-11-29 18:39:47 +0300

    iframe

--- src/App.js
+++ src/App.js
@@ -6,20 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+            <iframe src="http://localhost:8081/" style={{width: 800, height: 600}} frameBorder="0" />
       </div>
     );
   }

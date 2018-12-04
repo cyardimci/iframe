5a992b449480d5cf86a75940dbb6f6f06e289192
cyardimci <cyardimci@sabanciuniv.edu>
2018-12-04 17:53:30 +0300

    iframe url updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-            <iframe src="https://caroutlet.test.zero.codes" style={{width: 800, height: 600}} frameBorder="0" />
+            <iframe src="https://caroutlet.test.zero.codes/search" style={{width: 800, height: 600}} frameBorder="0" />
       </div>
     );
   }
